2a9e29c98aebba6cf81b340e051c2beb3168ea5e
jhs512 <jangka512@gmail.com>
2021-03-21 13:26:24 +0900

    게시물 컨트롤러 도입

--- src/com/sbs/example/javaTextBoard/Main.java
+++ src/com/sbs/example/javaTextBoard/Main.java
@@ -17,55 +17,25 @@ public class Main {
 }
 
 class App {
-	private List<Article> articles;
-	private int articlesLastId;
+	private ArticleController articleController;
+	public static Scanner sc;
 
 	public App() {
-		articles = new ArrayList<>();
-		articlesLastId = 0;
+		articleController = new ArticleController();
 	}
 
 	public void run() {
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 		while (true) {
 			System.out.printf("명령어) ");
 			String command = sc.nextLine().trim();
 
 			if (command.length() == 0)
 				continue;
-
 			if (command.equals("article add")) {
-				System.out.println("= 게시물 작성 - 시작 =");
-				System.out.printf("제목 : ");
-				String title = sc.nextLine().trim();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine().trim();
-				String regDate = Util.getNowDateStr();
-				int id = articlesLastId + 1;
-
-				Article article = new Article(id, regDate, title, body);
-				articlesLastId++;
-
-				articles.add(article);
-
-				System.out.printf("%d번 글이 생성되었습니다.\n", id);
-				System.out.println("= 게시물 작성 - 끝 =");
+				articleController.doAdd();
 			} else if (command.equals("article list")) {
-				System.out.println("= 게시물 리스트 - 시작 =");
-
-				System.out.println("번호 / 날짜 / 제목");
-
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
-
-					System.out.printf("%d / %s / %s\n", article.getId(), article.getRegDate(), article.getTitle());
-				}
-
-				/*
-				 * for (Article article : articles) { }
-				 */
-
-				System.out.println("= 게시물 리스트 - 끝 =");
+				articleController.showList();
 			} else if (command.equals("system exit")) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
@@ -74,6 +44,49 @@ class App {
 
 		sc.close();
 	}
+}
+
+class ArticleController {
+	private List<Article> articles;
+	private int articlesLastId;
+
+	public ArticleController() {
+		articles = new ArrayList<>();
+		articlesLastId = 0;
+	}
+
+	public void showList() {
+		System.out.println("= 게시물 리스트 - 시작 =");
+
+		System.out.println("번호 / 날짜 / 제목");
+
+		for (int i = articles.size() - 1; i >= 0; i--) {
+			Article article = articles.get(i);
+
+			System.out.printf("%d / %s / %s\n", article.getId(), article.getRegDate(), article.getTitle());
+		}
+
+		System.out.println("= 게시물 리스트 - 끝 =");
+	}
+
+	public void doAdd() {
+		System.out.println("= 게시물 작성 - 시작 =");
+		System.out.printf("제목 : ");
+		String title = App.sc.nextLine().trim();
+		System.out.printf("내용 : ");
+		String body = App.sc.nextLine().trim();
+		String regDate = Util.getNowDateStr();
+		int id = articlesLastId + 1;
+
+		Article article = new Article(id, regDate, title, body);
+		articlesLastId++;
+
+		articles.add(article);
+
+		System.out.printf("%d번 글이 생성되었습니다.\n", id);
+		System.out.println("= 게시물 작성 - 끝 =");
+	}
+
 }
 
 class Article {
